bce6643baecd2c7f80e4fd11167c7ba192030094
Kevin Villalobos <kevinv15v@gmail.com>
2020-02-28 19:26:57 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.chocolate", "3", "M", "black", "Calm", "0")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", "1", "M", "black", "Calm", "0")
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", "6", "M", "white", "Calm", "0")
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", "5", "M", "brown", "Calm", "0")
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", "8", "F", "black", "Calm", "0")
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", "9", "M", "black", "Calm", "0")
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", "7", "F", "black", "Calm", "0")
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, "2", "M", "black", "Calm", "0")
-
-
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "Black", "Hungry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "M", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabita", 5, "F", "Grizzly", "Angry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 9, "M", "Brown", "Happy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 1, "F", "Black", "Lovely", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 4, "M", "Grizzly", "Agressive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 8, "F", "Black", "Calm", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 10, "M", "Grizzly", "Sleepy", 1);
