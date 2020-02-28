fc4c9b0d9ddd05694bac9f6266dc975938f8098c
Kevin Villalobos <kevinv15v@gmail.com>
2020-02-28 19:22:15 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.chocolate", "3", "M", "black", "Calm", "0")
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", "1", "M", "black", "Calm", "0")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", "6", "M", "black", "Calm", "0")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", "6", "M", "white", "Calm", "0")
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", "5", "M", "brown", "Calm", "0")
 
