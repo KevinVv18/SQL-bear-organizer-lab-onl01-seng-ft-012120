7fc580db808f7ef0d1926421eeca10b9cb4ac900
Kevin Villalobos <kevinv15v@gmail.com>
2020-02-28 19:20:09 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.chocolate", "3", "M", "black", "Calm", "0")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", "3", "M", "black", "Calm", "0")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", "1", "M", "black", "Calm", "0")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", "3", "M", "black", "Calm", "0")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", "6", "M", "black", "Calm", "0")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", "3", "M", "black", "Calm", "0")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", "5", "M", "brown", "Calm", "0")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", "3", "M", "black", "Calm", "0")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", "8", "F", "black", "Calm", "0")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", "3", "M", "black", "Calm", "0")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", "9", "M", "black", "Calm", "0")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", "3", "M", "black", "Calm", "0")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", "7", "F", "black", "Calm", "0")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, "3", "M", "black", "Calm", "0")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, "2", "M", "black", "Calm", "0")
 
 
